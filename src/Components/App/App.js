ec6ebd5a1c0565d5c0bfb3be2029020b1951d03e
radu-lefter <68811789+radu-lefter@users.noreply.github.com>
2022-01-27 19:18:23 +0000

    tried to fix router validation problems

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,15 +1,17 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from '../Home/Home';
 import Search from '../Search/Search';
 
 function App() {
   return (
     <div className="App">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="search" element={<Search />} />
-      </Routes>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="search" element={<Search />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+// import { BrowserRouter } from 'react-router-dom';
 import App from './Components/App/App';
 import './index.css';
 
 // eslint-disable-next-line react/jsx-filename-extension
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>, document.getElementById('root'),
+  <App />, document.getElementById('root'),
 );
