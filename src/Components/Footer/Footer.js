d8ad3f3e8bde5b6a41478f08bf107272e077d510
radu-lefter <68811789+radu-lefter@users.noreply.github.com>
2022-02-01 22:08:39 +0000

    fixed foter tests

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -6,9 +6,9 @@ import { ReactComponent as Logo } from './logo2.svg';
 function Footer() {
   return (
     <footer className={styles.footer}>
-      <Link to="https://radulefter.co.uk/" className={styles.link1}>Radu Lefter</Link>
+      <Link to="profy.dev/employers" className={styles.link1}>Radu Lefter</Link>
       <Link to="/"><Logo /></Link>
-      <Link to="/" className={styles.link2}>Terms and Privacy</Link>
+      <Link to="/terms" className={styles.link2}>Terms and Privacy</Link>
     </footer>
   );
 }
