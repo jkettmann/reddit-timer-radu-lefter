233aa9e9db2638ac71093a0aa44875d4e985ae82
radu-lefter <68811789+radu-lefter@users.noreply.github.com>
2022-02-02 12:15:23 +0000

    Hero section (#4)
    * installed fonts Bitter and Montserrat
    
    * added @csstools/normalize to reset default browser styles
    
    * added global styles
    
    * added routes to home and search pages
    
    * fixed crlf issues
    
    * tried to fix crlf issues again
    
    * tried to fix validate-branch-home  issues
    
    * tried to fix router validation problems
    
    * implemented the header
    
    * Update App.js
    
    * Update Home.js
    
    * Update Search.js
    
    * changed header to pass tests
    
    * tried to resolve test error
    
    * tried to resolve test error 2nd time
    
    * implemented the footer
    
    * added footer tests
    
    * fixed linting errors
    
    * fixed foter tests
    
    * fixed testing errors
    
    * fixed link error
    
    * implemented hero section layout
    
    * implemented links to button and heatmap image
    
    * implemented tests for search button
    
    * made image responsive
    
    * fixed home page
    
    * fixed testing

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,43 +1,49 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import Header from '../Header/Header';
-
 import Footer from '../Footer/Footer';
-
+import styles from './Home.module.css';
 import table from './table.png';
 
 function Home() {
   return (
     <div>
       <Header />
-      <h1>This is the home page</h1>
-      <Link to="/search/javascript">Click to view our search page</Link>
-      <h1>
-        No reactions to your reddit posts?
-      </h1>
-      <h4>
-        Great timing, great results! Find the best time to post on your subreddit.
-      </h4>
-      {/* <button>
-        SHOW ME THE BEST TIME
-      </button> */}
-      <p>
-        r/javascript
-      </p>
-      <img src={table} alt="main table" />
-      <div id="how-it-works">
-        <h3>
-          How it works
-        </h3>
+      <div className={styles.main}>
+        <h1 className={styles.title}>
+          No reactions to your reddit posts?
+        </h1>
+        <div className={styles.subtitle}>
+          Great timing, great results! Find the best time to post on your subreddit.
+        </div>
+        <br />
+        <br />
+        <Link to="/search/javascript">
+          <button type="button" className={styles.button}>
+            SHOW ME THE BEST TIME
+          </button>
+        </Link>
+        <br />
+        <p>
+          r/javascript
+        </p>
+        <br />
+        <br />
+        <Link to="/search/javascript">
+          <img src={table} alt="main table" className={styles.image} />
+        </Link>
+        <section id="how-it-works">
+          <h2>
+            How it works
+          </h2>
+        </section>
+        <section id="about">
+          <h2>
+            About
+          </h2>
+        </section>
       </div>
-      <div id="about">
-        <h3>
-          About
-        </h3>
-      </div>
-
       <Footer />
-
     </div>
   );
 }
