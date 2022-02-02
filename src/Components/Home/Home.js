c945e6e0fa058c7712af05c07a3ed815e7d7feb6
radu-lefter <68811789+radu-lefter@users.noreply.github.com>
2022-02-02 18:09:38 +0000

    implemented the info section (#5)
    * implemented the info section
    
    * solved info section errors
    
    * removed links underline
    
    * changed ooloo to profy

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -14,7 +14,9 @@ function Home() {
           No reactions to your reddit posts?
         </h1>
         <div className={styles.subtitle}>
-          Great timing, great results! Find the best time to post on your subreddit.
+          <div>
+            Great timing, great results! Find the best time to post on your subreddit.
+          </div>
         </div>
         <br />
         <br />
@@ -32,15 +34,38 @@ function Home() {
         <Link to="/search/javascript">
           <img src={table} alt="main table" className={styles.image} />
         </Link>
-        <section id="how-it-works">
-          <h2>
-            How it works
-          </h2>
-        </section>
-        <section id="about">
-          <h2>
-            About
-          </h2>
+        <section className={styles.section}>
+          <article id="how-it-works" className={styles.subtitle}>
+            <h2 className={styles.headings}>
+              How it works
+            </h2>
+            <div>
+              • We find the 500 top posts from the past year for a subreddit.
+            </div>
+            <div>
+              • The data is visualized in a heatmap grouped by weekday and hour of the day.
+            </div>
+            <div>
+              • See immediately when to submit your reddit post.
+            </div>
+          </article>
+          <br />
+          <br />
+          <br />
+          <article id="about" className={styles.subtitle}>
+            <h2 className={styles.headings}>
+              About
+            </h2>
+            <div>
+              This app was created during a course on
+              <a href="https://profy.dev"> profy.dev </a>
+              with the goal
+              to implement a pixel-perfect real-world application with professional
+              workflows and tools like Kanban, Asana, Zeplin, GitHub, pull requests
+              and code reviews.
+              <a href="https://profy.dev/employers"> Click here for more information.</a>
+            </div>
+          </article>
         </section>
       </div>
       <Footer />
