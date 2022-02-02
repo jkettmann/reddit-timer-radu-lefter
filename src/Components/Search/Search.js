ba48125dd5211f091d8085b82dab9b7da3d7be7d
radu-lefter <68811789+radu-lefter@users.noreply.github.com>
2022-02-02 21:32:12 +0000

    updating parameters and input value through state

--- src/Components/Search/Search.js
+++ src/Components/Search/Search.js
@@ -1,18 +1,27 @@
-import React from 'react';
+import { useEffect, useState, React } from 'react';
 // import { Link } from 'react-router-dom';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import styles from './Search.module.css';
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
 
 function Search() {
-  const subred = window.location.href.split('/').pop();
+  const params = useParams();
   const navigate = useNavigate();
+  const [query, setQuery] = useState(params.javascript);
 
   function handleSubmit(event) {
     event.preventDefault();
     navigate(`/search/${event.target[0].value}`, { replace: true });
   }
+
+  function handleChange(event) {
+    setQuery(event.target.value);
+  }
+
+  useEffect(() => {
+    setQuery(params.javascript);
+  }, [params]);
 
   return (
     <div>
@@ -22,7 +31,7 @@ function Search() {
         <form onSubmit={handleSubmit}>
           <label htmlFor="subred">
             r /
-            <input type="text" id="subred" name="subred" defaultValue={subred} className={styles.input} />
+            <input type="text" id="subred" name="subred" value={query} className={styles.input} onChange={handleChange} />
           </label>
           <button type="submit" className={styles.button}>SEARCH</button>
         </form>
